42d5d326c5a53ef877a5a2f9c4d946a4085a763e
Jacob Jones <keneast24@gmail.com>
2026-04-28 12:01:25 -0400

    Add endpoint to retrieve book details by ISBN
    Implemented GET endpoint for book details by ISBN.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -34,9 +34,16 @@ public_users.get('/title/:title',function (req, res) {
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn; // Retrieve ISBN from request parameters
+    const book = books[isbn]; // Look up the book in your books database
+  
+    if (book) {
+      res.send(JSON.stringify(book, null, 4));
+    } else {
+      res.status(404).json({ message: "Book not found" });
+    }
+  });
 
 module.exports.general = public_users;
