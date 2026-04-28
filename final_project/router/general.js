db83ff783529eb602a9300f707678a0f6c4e6a13
Jacob Jones <keneast24@gmail.com>
2026-04-28 11:34:32 -0400

    Implement GET endpoint for book details by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,8 +18,7 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null, 4));
  });
   
 // Get book details based on author
